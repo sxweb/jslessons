8cb6ad84914cf3873f5ac492c214d82b6ad08725
sxweb <phpred@yandex.ru>
2020-12-17 15:06:54 +0300

    first homework

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 "use strict"
 
-let category = 'toys';
-let url = `https://someurl.com/${category}`;
-console.log(url);
+
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+}
+
+const questions = ['which last movie did you see?', 'what rating do you give him?']
+
+for (let i = 0; i < 2; i++) {
+    let movieName = prompt(questions[0], '');
+    let movieRating = prompt(questions[1]);
+    personalMovieDB.movies[movieName] = movieRating;
+    personalMovieDB.count++;
+
+}
+console.log(personalMovieDB.movies);
+console.log(personalMovieDB.count);
 
