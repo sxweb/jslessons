67bdf0d2097140604b646c67f94de2f3d02e0fa9
sxweb <phpred@yandex.ru>
2020-12-18 11:40:28 +0300

    second homework version 1

--- js/script.js
+++ js/script.js
@@ -1,23 +1,36 @@
 "use strict"
 
+let moviesCount = +prompt('How many movies did you see last time?');
 
 const personalMovieDB = {
-    count: 0,
+    count: moviesCount,
     movies: {},
     actors: {},
     genres: [],
     private: false
-}
+};
 
-const questions = ['which last movie did you see?', 'what rating do you give him?']
+const questions = ['which last movie did you see?', 'what rating do you give him?'];
 
-for (let i = 0; i < 2; i++) {
-    let movieName = prompt(questions[0], '');
-    let movieRating = prompt(questions[1]);
-    personalMovieDB.movies[movieName] = movieRating;
-    personalMovieDB.count++;
+for(let i = 0; i < 2; i++){
+    let firstQuestion,
+        secondQuestion;
+    do{
+        firstQuestion = prompt(questions[0], '');
+    }while(firstQuestion.length > 50 ||  firstQuestion === '');
+    
+    do{
+        secondQuestion = prompt(questions[1],'');
+    }while(secondQuestion ==='');
 
+    personalMovieDB.movies[firstQuestion] = secondQuestion;
+};
+
+if(personalMovieDB.count < 10){
+    console.log('you watched to many movies');
+}else if(personalMovieDB.count >10 && personalMovieDB.count < 30){
+    console.log('you watche anought conunt of movies');
+}else{
+    console.log('You watched so many movies!');
 }
 console.log(personalMovieDB.movies);
-console.log(personalMovieDB.count);
-
