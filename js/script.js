17785287808651e940e470b8e7c7d331cecfea9f
sxweb <phpred@yandex.ru>
2020-12-21 06:37:58 +0300

    functions homework started

--- js/script.js
+++ js/script.js
@@ -10,22 +10,40 @@ const personalMovieDB = {
     private: false
 };
 
-const questions = ['which last movie did you see?', 'what rating do you give him?'];
+const questions = ['which last movie did you see?', 'what rating do you give him?', 'which is your best genre?' +
+' 1- comedy, 2 - horror, 3 - action, 4 - melodrama (enter a number)'];
 
 for(let i = 0; i < 2; i++){
     let firstQuestion = '',
-        secondQuestion = '';
+        secondQuestion = '',
+        thirdQuestion = '';
     
     while(firstQuestion === null || firstQuestion.length > 50 ||  firstQuestion === ''){
         firstQuestion = prompt(questions[0], '');
-    };
+    }
     
     while(secondQuestion === null || secondQuestion ===''){
         secondQuestion = prompt(questions[1],'');
-    };
+    }
+
 
     personalMovieDB.movies[firstQuestion] = secondQuestion;
-};
+}
+
+personalMovieDB.genres = writeYourGenres(questions[2]);
+
+function writeYourGenres(question){
+    let genres = [];
+
+    for(let i = 0; i < 3; i++){
+        let thirdQuestion = '';
+        while(thirdQuestion === null || thirdQuestion ===''){
+            thirdQuestion = prompt(question,'');
+        }
+        genres[i] = thirdQuestion;
+    }
+    return genres;
+}
 
 if(personalMovieDB.count <= 10){
     console.log('you watched to many movies');
@@ -36,4 +54,6 @@ if(personalMovieDB.count <= 10){
 }else{
     console.log('Error');
 }
-console.log(personalMovieDB.movies);
+if(personalMovieDB.private === false){
+    console.log(personalMovieDB);
+}
