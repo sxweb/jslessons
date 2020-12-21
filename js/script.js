f41b8e65b762d9793a313eacdc5b240b6f3b950e
Yuriy <phpred@gmail.com>
2020-12-21 10:13:54 +0300

    functions homework

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 "use strict";
 
-let moviesCount = +prompt('How many movies did you see last time?');
+function start(){
+    let moviesCount = '';
+    while(moviesCount === 0 || moviesCount === '' || moviesCount === null || isNaN(moviesCount)){
+        moviesCount = +prompt('How many movies did you see last time?');
+    }
+    return moviesCount;
+}
+
+let moviesCount = start();
 
 const personalMovieDB = {
     count: moviesCount,
@@ -10,27 +18,29 @@ const personalMovieDB = {
     private: false
 };
 
-const questions = ['which last movie did you see?', 'what rating do you give him?', 'which is your best genre?' +
-' 1- comedy, 2 - horror, 3 - action, 4 - melodrama (enter a number)'];
+const questions = ['which last movie did you see?', 'what rating do you give him?', 'which is your best genre'];
 
-for(let i = 0; i < 2; i++){
-    let firstQuestion = '',
-        secondQuestion = '',
-        thirdQuestion = '';
+function setMovies(){
+    for(let i = 0; i < 2; i++){
+        let firstQuestion = '',
+            secondQuestion = ''
+        
+        while(firstQuestion === null || firstQuestion.length > 50 ||  firstQuestion === ''){
+            firstQuestion = prompt(questions[0], '');
+        }
+        
+        while(secondQuestion === null || secondQuestion ===''){
+            secondQuestion = prompt(questions[1],'');
+        }
     
-    while(firstQuestion === null || firstQuestion.length > 50 ||  firstQuestion === ''){
-        firstQuestion = prompt(questions[0], '');
+    
+        personalMovieDB.movies[firstQuestion] = secondQuestion;
     }
     
-    while(secondQuestion === null || secondQuestion ===''){
-        secondQuestion = prompt(questions[1],'');
-    }
-
-
-    personalMovieDB.movies[firstQuestion] = secondQuestion;
+    personalMovieDB.genres = writeYourGenres(questions[2]);
 }
 
-personalMovieDB.genres = writeYourGenres(questions[2]);
+setMovies();
 
 function writeYourGenres(question){
     let genres = [];
@@ -38,22 +48,31 @@ function writeYourGenres(question){
     for(let i = 0; i < 3; i++){
         let thirdQuestion = '';
         while(thirdQuestion === null || thirdQuestion ===''){
-            thirdQuestion = prompt(question,'');
+            thirdQuestion = prompt(question + ' ' + (i + 1) + '?','');
         }
         genres[i] = thirdQuestion;
     }
     return genres;
 }
 
-if(personalMovieDB.count <= 10){
-    console.log('you watched to many movies');
-}else if(personalMovieDB.count >10 && personalMovieDB.count < 30){
-    console.log('you watched anought conunt of movies');
-}else if(personalMovieDB.count >= 30){
-    console.log('You watched so many movies!');
-}else{
-    console.log('Error');
+function calculateUserRating(){
+    if(personalMovieDB.count <= 10){
+        console.log('you watched to many movies');
+    }else if(personalMovieDB.count >10 && personalMovieDB.count < 30){
+        console.log('you watched anought conunt of movies');
+    }else if(personalMovieDB.count >= 30){
+        console.log('You watched so many movies!');
+    }else{
+        console.log('Error');
+    }
 }
-if(personalMovieDB.private === false){
-    console.log(personalMovieDB);
+
+calculateUserRating();
+
+function showMyDB(){
+    if(personalMovieDB.private === false){
+        console.log(personalMovieDB);
+    }
 }
+
+showMyDB();
