bd5f5318babcd812c99e5a0048656c9d79f2a49a
sxweb <phpred@yandex.ru>
2020-12-30 09:47:16 +0300

    oop and methods  homework

---
+++ js/oop.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function (){
+        console.log('hello');
+    }
+}
+
+const john = {
+    health: 100
+}
+//deprecated approach
+/*john.__proto__ = soldier;
+john.sayHello();*/
+
+Object.setPrototypeOf(john, soldier);
+//john.sayHello();
+
+const yuriy = Object.create(soldier);
+yuriy.sayHello();
--- js/script.js
+++ js/script.js
@@ -1,78 +1,83 @@
 "use strict";
 
-function start(){
-    let moviesCount = '';
-    while(moviesCount === 0 || moviesCount === '' || moviesCount === null || isNaN(moviesCount)){
-        moviesCount = +prompt('How many movies did you see last time?');
-    }
-    return moviesCount;
-}
-
-let moviesCount = start();
+const questions = ['which last movie did you see?', 'what rating do you give him?', 'which is your best genre'];
 
 const personalMovieDB = {
-    count: moviesCount,
+    count: '',
     movies: {},
     actors: {},
     genres: [],
-    private: false
+    private: false,
+
+    start: function(){
+        while(this.count === 0 || this.count === '' || this.count === null || isNaN(this.count)){
+            this.count = +prompt('How many movies did you see last time?');
+        }
+    },
+
+    calculateUserRating: function (){
+        if(this.count <= 10){
+            console.log('you watched to many movies');
+        }else if(this.count >10 && this.count < 30){
+            console.log('you watched anought conunt of movies');
+        }else if(this.count >= 30){
+            console.log('You watched so many movies!');
+        }else{
+            console.log('Error');
+        }
+    },
+
+    printResult: function(){
+        if(this.private === false){
+            console.log(this);
+        }
+    },
+
+    writeYourGenres: function(question){
+        for(let i = 0; i < 3; i++){
+            let thirdQuestion = '';
+            while(thirdQuestion === null || thirdQuestion ===''){
+                thirdQuestion = prompt(question + ' ' + (i + 1) + '?','');
+            }
+            this.genres[i] = thirdQuestion;
+        }
+    },
+
+    setMovies: function (){
+        for(let i = 0; i < 2; i++){
+            let firstQuestion = '',
+                secondQuestion = ''
+
+            while(firstQuestion === null || firstQuestion.length > 50 ||  firstQuestion === ''){
+                firstQuestion = prompt(questions[0], '');
+            }
+
+            while(secondQuestion === null || secondQuestion ===''){
+                secondQuestion = prompt(questions[1],'');
+            }
+
+
+            this.movies[firstQuestion] = secondQuestion;
+        }
+    },
+
+    toggleVisibilityMyDB: function (){
+        this.private = !this.private;
+    },
+
+    printFavouriteGenres: function(){
+        this.genres.forEach((item, i) => {
+            console.log(`Your favorite genre ${i + 1} is: ${item}`);
+        });
+    }
 };
 
-const questions = ['which last movie did you see?', 'what rating do you give him?', 'which is your best genre'];
+personalMovieDB.start();
 
-function setMovies(){
-    for(let i = 0; i < 2; i++){
-        let firstQuestion = '',
-            secondQuestion = ''
-        
-        while(firstQuestion === null || firstQuestion.length > 50 ||  firstQuestion === ''){
-            firstQuestion = prompt(questions[0], '');
-        }
-        
-        while(secondQuestion === null || secondQuestion ===''){
-            secondQuestion = prompt(questions[1],'');
-        }
-    
-    
-        personalMovieDB.movies[firstQuestion] = secondQuestion;
-    }
-    
-    personalMovieDB.genres = writeYourGenres(questions[2]);
-}
+personalMovieDB.setMovies();
+personalMovieDB.writeYourGenres(questions[2]);
 
-setMovies();
+personalMovieDB.calculateUserRating();
 
-function writeYourGenres(question){
-    let genres = [];
-
-    for(let i = 0; i < 3; i++){
-        let thirdQuestion = '';
-        while(thirdQuestion === null || thirdQuestion ===''){
-            thirdQuestion = prompt(question + ' ' + (i + 1) + '?','');
-        }
-        genres[i] = thirdQuestion;
-    }
-    return genres;
-}
-
-function calculateUserRating(){
-    if(personalMovieDB.count <= 10){
-        console.log('you watched to many movies');
-    }else if(personalMovieDB.count >10 && personalMovieDB.count < 30){
-        console.log('you watched anought conunt of movies');
-    }else if(personalMovieDB.count >= 30){
-        console.log('You watched so many movies!');
-    }else{
-        console.log('Error');
-    }
-}
-
-calculateUserRating();
-
-function showMyDB(){
-    if(personalMovieDB.private === false){
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB();
+personalMovieDB.printResult();
+personalMovieDB.printFavouriteGenres();
