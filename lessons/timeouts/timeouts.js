b559ffeb02c7500e26fca4df8f6b8f3450d8e568
sxweb <phpred@yandex.ru>
2021-01-18 19:20:57 +0300

    some fixes

--- lessons/timeouts/timeouts.js
+++ lessons/timeouts/timeouts.js
@@ -21,3 +21,22 @@ button.addEventListener('click', (e)=>{
     },10);
 });
 
+//const someDate = new Date();
+//const someDate = new Date('2020-05-01');
+//const someDate = new Date(0);
+//const someDate = new Date(-101000000000);
+const someDate = new Date();
+console.log(someDate.getFullYear());
+console.log(someDate.getDate());
+console.log(someDate.getDay());
+console.log(someDate.getHours());
+console.log(someDate.getUTCHours());
+console.log(someDate.getTime());
+
+let start = new Date();
+for(let i = 0; i < 10000000; i ++){
+    let some = i **3;
+}
+let end = new Date();
+console.log(end - start);
+
