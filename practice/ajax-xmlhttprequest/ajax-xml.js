f35f62653bfb94f2cc4837dbf6d267da27132215
Yuriy <phpred@gmail.com>
2021-02-01 10:35:07 +0300

    one more time practice, ajax xmlhttprequest

--- practice/ajax-xmlhttprequest/ajax-xml.js
+++ practice/ajax-xmlhttprequest/ajax-xml.js
@@ -4,16 +4,20 @@ const count = document.querySelector('#count'),
     result = document.querySelector('#result');
 
 count.addEventListener('input', ()=>{
-   const request = new XMLHttpRequest();
-   request.open('GET', 'practice/ajax-xmlhttprequest/data.json');
-   request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-   request.send();
+    
+    const request = new XMLHttpRequest();
+    request.open('GET', 'practice/ajax-xmlhttprequest/data.json');
+    request.setRequestHeader('Content-type', 'application/ajax; charset=utf-8');
+    request.send();
 
-   request.addEventListener('load', ()=>{
-      if(request.status === 200){
-          console.log(request.response);
-          const response = JSON.parse(request.response);
-          result.value = (count.value / response.currency.usd).toFixed(2);
-      }
-   });
+    request.addEventListener('load', ()=>{
+        const response = JSON.parse(request.response);
+        console.log(response.currency.usd);
+        if(request.status === 200){
+            result.value = (count.value / response.currency.usd).toFixed(2);
+        }else{
+            result.value = 'something wrong';
+        }
+    });
+
 });
