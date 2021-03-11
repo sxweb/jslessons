eb0ecfdb5a3093ad1bb8edf28d8c05214cd60809
sxweb <phpred@yandex.ru>
2021-03-11 18:50:38 +0300

    callories calc practice part 2

--- js/script.js
+++ js/script.js
@@ -367,6 +367,35 @@ let sex = 'female',
     age,
     ratio = 1.35;
 
+if(localStorage.getItem('ratio')){
+    ratio = localStorage.getItem('ratio');
+}else{
+    ratio = 1.35;
+}
+
+if(localStorage.getItem('sex')){
+    sex = localStorage.getItem('sex');
+}else{
+    sex = 'female';
+}
+
+function setActiveElement(parent, activeClass){
+    const elements = document.querySelectorAll(`${parent} div`);
+
+    elements.forEach((element) =>{
+        element.classList.remove(activeClass);
+        if(element.getAttribute('id') === localStorage.getItem('sex')){
+            element.classList.add(activeClass);
+        }
+        if(element.getAttribute('data-ratio') === localStorage.getItem('ratio')){
+            element.classList.add(activeClass);
+        }
+    });
+}
+
+setActiveElement('#gender', 'calculating__choose-item_active');
+setActiveElement('.calculating__choose_big', 'calculating__choose-item_active');
+
 function  calcTotal(){
     if(!sex || !height || !weight || !age || !ratio ){
         result.textContent = '___';
@@ -387,8 +416,10 @@ function getStaticInformation(parent, activeClass){
         element.addEventListener('click', (e)=>{
             if(e.target.getAttribute('data-ratio')){
                 ratio = e.target.getAttribute('data-ratio');
+                localStorage.setItem('ratio', e.target.getAttribute('data-ratio'));
             }else{
                 sex = e.target.getAttribute('id');
+                localStorage.setItem('sex', e.target.getAttribute('id'));
             }
 
             elements.forEach(element =>{
@@ -407,6 +438,11 @@ function getDynamicInformation(selector){
     const input = document.querySelector(selector);
 
     input.addEventListener('input', ()=>{
+        if(input.value.match(/\D/g)){
+            input.style.border = '1px solid red';
+        }else{
+            input.style.border = 'none';
+        }
         switch (input.getAttribute('id')){
             case 'height':
                 height = +input.value;
